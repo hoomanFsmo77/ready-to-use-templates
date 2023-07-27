6e67908e433819d135a9c5e062fba4af0c8f66b1
hooman mousavi <hoomanmousavi77@gmail.com>
2023-07-27 15:32:43 +0330

    nuxt templates updated

--- Nuxt template/nuxt.config.ts
+++ Nuxt template/nuxt.config.ts
@@ -1,25 +1,26 @@
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    build:{
-        transpile:[]
-    },
-    css:['~/assets/App.scss'],
-    app:{
-        rootId:'v-app',
-        rootTag:'main',
-        head:{
-            title:'my website',
-            meta: [
-                { name: 'viewport', content: 'width=device-width ,initial-scale=1.0' },
-                { name: 'description', content: 'welcome to My project' },
-                { name: 'keyword', content: 'HTML,CSS,Js developer' },
-                { "http-equiv": 'X-UA-Compatible', content: 'ie=edge' },
-            ],
-            bodyAttrs:{}
-        }
-    },
-    srcDir: './src',
-    modules: [
-        '@pinia/nuxt','@nuxt/image-edge'
-    ],
+  devtools: { enabled: true },
+  build:{
+    transpile:[]
+  },
+  css:['~/assets/App.scss'],
+  app:{
+    rootId:'v-app',
+    rootTag:'main',
+    head:{
+      title:'my website',
+      meta: [
+        { name: 'viewport', content: 'width=device-width ,initial-scale=1.0' },
+        { name: 'description', content: 'welcome to My project' },
+        { name: 'keyword', content: 'HTML,CSS,Js developer' },
+        { "http-equiv": 'X-UA-Compatible', content: 'ie=edge' },
+      ],
+      bodyAttrs:{}
+    }
+  },
+  srcDir: './src',
+  modules: [
+    '@pinia/nuxt','@nuxt/image-edge'
+  ],
 })
